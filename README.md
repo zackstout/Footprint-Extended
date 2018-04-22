293f91a91f93f7e1c7e5050d0f96eca1c1137cb4
Zack Stout <zackstout@gmail.com>
2018-04-21 23:10:10 -0500

    adding clean number function and mapping out ng-changes"

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
 # Project Footprint: Extended
 
 ## Changes made:
-- [x] Refactored CSV parsing and footprint computation to minimize code-duplication.
-- [x] That is, consolidated trial upload, footprint upload and footprint edit to follow one set of functions.
+- [x] Refactored CSV parsing and footprint computation to minimize code-duplication. (Consolidated trial upload, footprint upload and footprint edit to follow one set of functions.)
 - [x] Refactored routers into multiple files.
 - [x] Fixed bug where CSV was not being properly parsed (because of extra date column).
 - [x] Fixed bug where conversion to Imperial measurement required integer value.
--- server/public/scripts/controllers/calc.controller.js
+++ server/public/scripts/controllers/calc.controller.js
@@ -3,6 +3,34 @@ myApp.controller('CalcController', function (UserService, $http) {
     console.log('CalcController created');
     var vm = this;
 
+    //Hmm I wonder whether ng-changes can be chained, so that changing inputs could change one output, whose change could trigger other changes. Probably!
+    // NO! Thwarted! "The ng-change event is only triggered if there is a actual change in the input value, and not if the change was made from a JavaScript."
+
+    function cleanNumber(n) {
+      const str = n.toString();
+      const dot = str.includes('.');
+      let res = '';
+      for (let i=0; i < str.length; i++) {
+        const index = str.length - 1 - i;
+        res = str[index] + res;
+        // wow why can't i think of a better way to do this....
+        if (dot) {
+          if (i > 2) {
+            if (i % 3 == 2 && i !== str.length - 1) {
+              res = ',' + res;
+            }
+          }
+        } else {
+          if (i % 3 == 2 && i !== str.length - 1) {
+            res = ',' + res;
+          }
+        }
+      }
+      return res;
+    }
+
+    console.log(cleanNumber(12434));
+
     // going to have to fix this, probably (multiple users problem):
     vm.progress = 0;
 
@@ -17,56 +45,56 @@ myApp.controller('CalcController', function (UserService, $http) {
 
     // We're going to have to attach this to each user's session -- but they have't even logged in yet... Do we force them to?
     vm.progress = 0;
-    
+
     vm.submit = function(prog) {
       // increment hide/show progress:
       // is this going to break if multiple users doing it at once? Probably.
       vm.progress = prog + 1;
 
-      //update values of outputs:
+      //update values of outputs -- Or maybe this is unnecessary given the ng-changes?:
       vm.dailyLiters = vm.calculateDailyLiters();
 
       // Maybe we *make* them click submit on 3 before showing values? Then we can save to DB.
-      console.log(prog);
+      // console.log(prog);
     };
 
 
-    // Oh no we're going to need to pass each to the ng-change; we defintely have to refactor this.
     // Calculations for outputs:
     vm.calculateDieselUse = () => (vm.size * vm.hours * vm.load/100).toFixed(2); // Oh big mistake, *don't* divide by 24. Because we want kWh/day.
-    vm.changeDieselUse = () => { vm.dieselUsage = vm.calculateDieselUse(); };
+    vm.calculateDailyLiters = () => (24 * (vm.dieselUsage / 15)).toFixed(2);
+    vm.calculateMonthlyCost = () => (30 * vm.dailyLiters * vm.costPerLiter).toFixed(2);
+    vm.calculateAnnualCost = () => (vm.month * 12).toFixed(2);
+    // 2.8 kg carbon per liter. 3.8 liters per gallon.
+    vm.calculateCarbon = () => (vm.dailyLiters * 3.8 * 2.8 * 365).toFixed(0);
+    // I'm doing the strange /25 + /500 thing because 1000 gets taken to 42.
+    var sunFactor = 80; // 1000/125, by experiment
+    var overspecFactor = 240; // 1000/42, by experiment
+    vm.calculateSolarSize = () => ((vm.dieselUsage/25 + vm.dieselUsage/500) + ((100 - vm.dayPower)/100 * vm.dieselUsage / sunFactor) + (vm.overspec * vm.dieselUsage / overspecFactor)).toFixed(1);
+    vm.calculateSolarCost = () => (vm.solarSize * 1000 * vm.budget).toFixed(2);
+    vm.calculateCoverTime = () => (vm.solarCost / (vm.dailyLiters * vm.costPerLiter)).toFixed(0);
+    // we should just put dailyCost in its over variable but whatever
+    vm.calculateSavings = () => ((5 * 365 - vm.coverTime) * vm.dailyLiters * vm.costPerLiter).toFixed(2);
+
+    // Handle changes to input fields:
+    vm.changeDieselUse = () => {
+      vm.dieselUsage = vm.calculateDieselUse();
+
+
+
+    };
 
     // This is gallons, not liters, per day, and only a rough estimate based on the chart (http://www.dieselserviceandsupply.com/Diesel_Fuel_Consumption.aspx):
-    vm.calculateDailyLiters = () => (24 * (vm.dieselUsage / 15)).toFixed(2);
-    // Ok let's definitely CURRY this. Actually might not be worth..? Unsure:
     // This should also be called when any of first 3 inputs are changed:
     vm.changeDieselCost = () => {
       vm.dailyLiters = vm.calculateDailyLiters();
       vm.changeMonthlyCost();
     };
 
-    vm.calculateMonthlyCost = () => (30 * vm.dailyLiters * vm.costPerLiter).toFixed(2);
     vm.changeMonthlyCost = () => {
       vm.month = vm.calculateMonthlyCost();
       vm.year = vm.calculateAnnualCost();
     };
 
-    vm.calculateAnnualCost = () => (vm.month * 12).toFixed(2);
-
-    // 2.8 kg carbon per liter. 3.8 liters per gallon.
-    vm.calculateCarbon = () => (vm.dailyLiters * 3.8 * 2.8 * 365).toFixed(0);
-
-    // I'm doing the strange /25 + /500 thing because 1000 gets taken to 42.
-    var sunFactor = 80; // 1000/125, by experiment
-    var overspecFactor = 240; // 1000/42, by experiment
-    vm.calculateSolarSize = () => ((vm.dieselUsage/25 + vm.dieselUsage/500) + ((100 - vm.dayPower)/100 * vm.dieselUsage / sunFactor) + (vm.overspec * vm.dieselUsage / overspecFactor)).toFixed(1);
-
-    vm.calculateSolarCost = () => (vm.solarSize * 1000 * vm.budget).toFixed(2);
-
-    vm.calculateCoverTime = () => (vm.solarCost / (vm.dailyLiters * vm.costPerLiter)).toFixed(0);
-
-    // we should just put dailyCost in its over variable but whatever
-    vm.calculateSavings = () => ((5 * 365 - vm.coverTime) * vm.dailyLiters * vm.costPerLiter).toFixed(2);
 
 
     // Initialize outputs:
